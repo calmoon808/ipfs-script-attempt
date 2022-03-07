c8a4809f823ea2af01c8cf7d05d1a84b195c930d
cmoon <calmoon808@gmail.com>
2022-03-06 20:24:56 -1000

    updated syntax for ipfs-http-client

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-const { create } = require('ipfs-http-client');
-const express = require('express');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const req = require('express/lib/request');
+const { create } = require("ipfs-http-client");
+const express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 const fs = require('fs');
 
-const ipfs = create({ host:'localhost', port: '5001', protocol: 'http'});
+const ipfs = create();
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.post('/upload', (req,res) => {
+app.post('/upload', (req, res) => {
     const file = req.files.file;
     const fileName = req.body.fileName;
-    const filePath = 'files/' + fileName; 
+    const filePath = 'files/' + fileName;
 
     file.mv(filePath, async (err) => {
         if (err) {
-            console.log('Error: failed to download the file');
+            console.log("Error: failed to download the file");
             return res.status(500).send(err);
         }
 
         const fileHash = await addFile(fileName, filePath);
         fs.unlink(filePath, (err) => {
             if (err) console.log(err);
-        });
+        })
 
-        res.render('upload', { fileName, fileHash });
+        res.render("upload", { fileName, fileHash });
     })
 });
 
 const addFile = async (fileName, filePath) => {
     const file = fs.readFileSync(filePath);
-    const fileAdded = await ipfs.add({path: fileName, content: file});
+    const fileAdded = await ipfs.add({ path: fileName, content: file });
     const fileHash = fileAdded[0].hash;
 
     return fileHash;
-}
+};
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000')
+    console.log('Server is listening on port 3000');
 });
